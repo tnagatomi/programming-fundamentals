131e4c233856c9e77d140ac29fc8eee280b1c0b3
tommy <tommyt6073@gmail.com>
2018-02-13 17:57:34 +0900

    add: ex04_6 tsuru_no_ashi関数のテスト

--- chap04/ex04_6.ml
+++ chap04/ex04_6.ml
@@ -1,3 +1,8 @@
 (* 目的：鶴の数 x  に応じた足の本数を計算する *)
 (* tsuru_no_ashi : int -> int *)
 let tsuru_no_ashi x = 0
+
+(* テスト *)
+let test1 = tsuru_no_ashi 0 = 0
+let test2 = tsuru_no_ashi 1 = 2
+let test3 = tsuru_no_ashi 3 = 6
