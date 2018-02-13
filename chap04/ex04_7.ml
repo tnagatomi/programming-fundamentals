c5a2dd500972dd07b4ad9281f393852e71ce8fe3
tommy <tommyt6073@gmail.com>
2018-02-13 18:09:13 +0900

    add: ex04_6 tsurukame_no_ashi関数のテスト

--- chap04/ex04_7.ml
+++ chap04/ex04_7.ml
@@ -2,3 +2,7 @@
 (* tsurukame_no_ashi : int -> int -> int *)
 let tsurukame_no_ashi tsuru kame = 0
 
+(* テスト *)
+let test1 = tsurukame_no_ashi 0 0 = 0
+let test2 = tsurukame_no_ashi 1 1 = 6
+let test3 = tsurukame_no_ashi 3 4 = 22
