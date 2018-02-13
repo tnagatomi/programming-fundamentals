fb116144a6ece0a5b4848fcb1af161c907935b3b
tommy <tommyt6073@gmail.com>
2018-02-13 18:15:20 +0900

    add: ex04_6 tsurukame_no_ashi関数の完成

--- chap04/ex04_7.ml
+++ chap04/ex04_7.ml
@@ -1,6 +1,6 @@
 (* 目的：鶴の数 tsuru 亀の数 kame に応じた足の数の合計を計算する *)
 (* tsurukame_no_ashi : int -> int -> int *)
-let tsurukame_no_ashi tsuru kame = 0
+let tsurukame_no_ashi tsuru kame = tsuru * 2 + kame * 4
 
 (* テスト *)
 let test1 = tsurukame_no_ashi 0 0 = 0
