601d0bb4bb883d77718e1327760cc41c4a972aa1
tommy <tommyt6073@gmail.com>
2018-02-13 18:32:11 +0900

    add: ex04_8 tsurukame関数のテスト

--- chap04/ex04_8.ml
+++ chap04/ex04_8.ml
@@ -1,3 +1,8 @@
 (* 目的：鶴と亀の合計の数 gokei  足の数の合計 ashi に応じた鶴の数を計算する *)
 (* tsurukame : int -> int -> int *)
 let tsurukame gokei ashi = (gokei * 4 - ashi) / 2 
+
+(* テスト *)
+let test1 = tsurukame 0 0 = 0
+let test2 = tsurukame 1 2 = 1
+let test3 = tsurukame 3 8 = 2
