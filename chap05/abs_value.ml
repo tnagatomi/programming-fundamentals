27e5fed21be35e0a93c52b93d97a1585e0ee02f1
tommy <tommyt6073@gmail.com>
2018-02-18 09:41:06 +0900

    add: 5.5 abs_value の本体

--- chap05/abs_value.ml
+++ chap05/abs_value.ml
@@ -1,6 +1,8 @@
 (* 目的：受け取った実数 x の絶対値を計算する *)
 (* abs_value : float -> float *)
-let abs_value x = 0.0
+let abs_value x =
+  if x > 0.0 then x
+             else -. x
 
 (* テスト *)
 let test1 = abs_value 2.8 = 2.8
