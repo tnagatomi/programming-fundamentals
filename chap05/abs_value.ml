6ff0c68b9a802274b851d2998066d384e2423f1d
tommy <tommyt6073@gmail.com>
2018-02-18 09:38:43 +0900

    add: 5.5 abs_value の目的とテスト

---
+++ chap05/abs_value.ml
@@ -0,0 +1,8 @@
+(* 目的：受け取った実数 x の絶対値を計算する *)
+(* abs_value : float -> float *)
+let abs_value x = 0.0
+
+(* テスト *)
+let test1 = abs_value 2.8 = 2.8
+let test2 = abs_value (-2.8) = 2.8
+let test3 = abs_value 0.0 = 0.0
