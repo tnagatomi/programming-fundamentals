1d87416ba78cac859c8a1410859b0b77aa861695
tommy <tommyt6073@gmail.com>
2018-02-18 09:53:57 +0900

    add: ex05_2 jikan の目的とテスト

---
+++ chap05/ex05_2.ml
@@ -0,0 +1,12 @@
+(* 目的：受け取った時間 t が午前か午後かを返す *)
+(*  : int -> string *)
+let jikan t =
+  "午前"
+
+(* テスト *)
+let test1 = jikan 0 = "午前"
+let test2 = jikan 1 = "午前"
+let test3 = jikan 11 = "午前"
+let test4 = jikan 12 = "午後"
+let test5 = jikan 13 = "午後"
+let test6 = jikan 23 = "午後"
