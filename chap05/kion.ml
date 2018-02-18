a3cd470c917339c9293304da44936b09bfa5f20f
tommy <tommyt6073@gmail.com>
2018-02-18 10:05:34 +0900

    add: ex05_2 kaiteki の本体とテスト

--- chap05/kion.ml
+++ chap05/kion.ml
@@ -8,3 +8,15 @@ let test2 = kion 15 = "快適"
 let test3 = kion 20 = "快適"
 let test4 = kion 25 = "快適"
 let test5 = kion 28 = "普通"
+
+(* 目的：現在の気温 t が 15 以上 25 以下かどうかをチェックする *)
+(* kaiteki : int -> bool *)
+let kaiteki t =
+  15 <= t && t <= 25
+
+(* テスト *)
+let test1 = kaiteki  7 = false
+let test2 = kaiteki 15 = true
+let test3 = kaiteki 20 = true
+let test4 = kaiteki 25 = true
+let test5 = kaiteki 28 = false
