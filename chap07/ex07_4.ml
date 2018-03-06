bf9f7ae8b708ff6d5d8b0bfb1dd9272b12864c39
tommy <tommyt6073@gmail.com>
2018-03-06 21:07:32 +0900

    add: ex07_4 chuten のテンプレート

--- chap07/ex07_4.ml
+++ chap07/ex07_4.ml
@@ -1,6 +1,8 @@
 (* 目的：x座標とy座標の組で表された表面座標をふたつ受け取ったらその中点の座標を返す *)
 (* chuten : float * float -> float * float -> float * float *)
-let chuten zahyo1 zahyo2 = (0.0, 0.0)
+let chuten zahyo1 zahyo2 = match zahyo1 with
+  (x1, y1) -> match zahyo2 with
+    (x2, y2) -> ((x1 +. x2) /. 2.0, (y1 +. y2) /. 2.0)
 
 (* テスト *)
 let test1 = chuten (0.0, 0.0) (0.0, 0.0) = (0.0, 0.0)
