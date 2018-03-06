ec614c8a4f86db7c08fd75ddbf485b6eaa4188b6
tommy <tommyt6073@gmail.com>
2018-03-06 20:53:05 +0900

    add: ex07_3 taisho_x のテンプレート

--- chap07/ex07_3.ml
+++ chap07/ex07_3.ml
@@ -1,6 +1,7 @@
 (* 目的：x 座標と y 座標の組みで表された平面座標を受け取ったら x 軸について対象な点の座標を返す *)
 (* taisho_x : float * float -> float * float *)
-let taisho_x pair = (0., 0.)
+let taisho_x pair = match pair with
+  (x, y) -> (x, -. y)
 
 (* テスト *)
 let test1 = taisho_x (0., 0.) = (0., 0.)
