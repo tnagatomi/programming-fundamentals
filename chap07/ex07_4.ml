9f99f1f4102773567400fae8bb94c78466498195
tommy <tommyt6073@gmail.com>
2018-03-06 21:01:15 +0900

    add: ex07_4 chuten の目的とテスト

---
+++ chap07/ex07_4.ml
@@ -0,0 +1,9 @@
+(* 目的：x座標とy座標の組で表された表面座標をふたつ受け取ったらその中点の座標を返す *)
+(* chuten : float * float -> float * float -> float * float *)
+let chuten zahyo1 zahyo2 = (0.0, 0.0)
+
+(* テスト *)
+let test1 = chuten (0.0, 0.0) (0.0, 0.0) = (0.0, 0.0)
+let test2 = chuten (0.0, 0.0) (1.0, 2.0) = (0.5, 1.0) 
+let test3 = chuten (2.3, 5.1) (7.6, 1.7) = (4.95, 3.4) 
+let test4 = chuten (-3.8, -2.4) (3.4, -1.2) = (-0.2, -1.8) 
