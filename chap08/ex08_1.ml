7324f8446269e99190e483387076e70f5ab4fe47
tommy <tommyt6073@gmail.com>
2018-03-09 19:52:51 +0900

    add: ex08_1

---
+++ chap08/ex08_1.ml
@@ -0,0 +1,9 @@
+type book_t = {
+  title : string;
+  author : string;
+  publisher : string;
+  price : int;
+  isbn : string;
+}
+
+let a = {title = "a"; author = "b"; publisher = "c"; price = 100; isbn = "12345"}
