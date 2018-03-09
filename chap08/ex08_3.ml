d772d6a4d0e2c011dea93273d07fce2b312027f6
tommy <tommyt6073@gmail.com>
2018-03-09 19:57:31 +0900

    add: ex08_3

---
+++ chap08/ex08_3.ml
@@ -0,0 +1,15 @@
+type person_t = {
+  name : string;
+  height : int;
+  weight : int;
+  birthday : string;
+  blood_type : string;
+}
+
+let a = {
+  name = "a";
+  height = 182;
+  weight = 85;
+  birthday = "1985/07/03";
+  blood_type = "A";
+}
