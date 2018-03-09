e09515f00a2ef1730301a2440cd5ae5026f9b771
tommy <tommyt6073@gmail.com>
2018-03-10 07:53:15 +0900

    fix: ex08_6 hyoji の区切りを"、"に統一

--- chap08/ex08_6.ml
+++ chap08/ex08_6.ml
@@ -4,13 +4,13 @@ let ekimei1 = {kanji = "赤坂駅"; kana = "あかさかえき"; romaji = "akasa
 let ekimei2 = {kanji="渋谷"; kana="しぶや"; romaji="shibuya"; shozoku="銀座線"}
 let ekimei3 = {kanji="大手町"; kana="おおてまち"; romaji="otemachi"; shozoku="千代田線"}
 
-(* 目的：ekimei_t 型のデータを受け取り「路線名, 駅名（かな）」の形式の文字列を返す *)
+(* 目的：ekimei_t 型のデータを受け取り「路線名、駅名（かな）」の形式の文字列を返す *)
 (* hyoji : ekimei_t -> string *)
 let hyoji ekimei = match ekimei with
   {kanji = kj; kana = kn; romaji = r; shozoku = s} ->
     s ^ "、" ^ kj ^ "（" ^ kn ^ "）"
 
 (* テスト *)
-let test1 = hyoji ekimei1 = "地下鉄空港線, 赤坂駅（あかさかえき）"
+let test1 = hyoji ekimei1 = "地下鉄空港線、赤坂駅（あかさかえき）"
 let test2 = hyoji ekimei2 = "銀座線、渋谷（しぶや）"
 let test3 = hyoji ekimei3 = "千代田線、大手町（おおてまち）"
