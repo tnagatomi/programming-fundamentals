162ac6f846971de5dfe7d680490ee0b21e1d616a
tommy <tommyt6073@gmail.com>
2018-06-23 17:38:03 +0900

    fix: ex10_1 insert のテストを修正

--- ex10_1.ml
+++ ex10_1.ml
@@ -3,6 +3,6 @@
 let insert lst n = []
 
 (* テスト *)
-let test1 = insert 1 [] = [1]
-let test2 = insert 4 [1] = [1; 4]
-let test3 = insert 4 [1; 2; 5] = [1; 2; 4; 5]
+let test1 = insert [] 1 = [1]
+let test2 = insert [1] 4 = [1; 4]
+let test3 = insert [1; 3; 4; 7; 8] 5 = [1; 3; 4; 5; 7; 8]
