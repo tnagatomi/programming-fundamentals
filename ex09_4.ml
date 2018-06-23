975339141499d0c16ea0c58e38eccf2cdcbc4d6d
tommy <tommyt6073@gmail.com>
2018-06-23 10:19:02 +0900

    add: ex09_4 length の目的

---
+++ ex09_4.ml
@@ -0,0 +1,9 @@
+(* int list は 
+     - []              空リスト、あるいは 
+     - first :: rest   最初の要素が first で残りのリストが rest 
+                       （rest が自己参照のケース） 
+   という形 *) 
+
+(* 目的：受け取ったリスト lst の長さを返す *)
+(* length : int lst -> int *)
+let length lst = 0
