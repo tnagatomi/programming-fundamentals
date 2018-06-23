e185389bad52b7b6e087688a013ce2a99e8c0b69
tommy <tommyt6073@gmail.com>
2018-06-23 09:47:09 +0900

    add: 9.4 contain_zero の目的とテスト

---
+++ chap09/contain_zero.ml
@@ -0,0 +1,10 @@
+(* 目的：受け取ったリスト lst に 0 が含まれているかを調べる *)
+(* contain_zero : int list -> bool *)
+let contain_zero lst = false
+
+(* テスト *)
+let test1 = contain_zero [] = false
+let test2 = contain_zero [0; 2] = true 
+let test3 = contain_zero [1; 2] = false 
+let test4 = contain_zero [1; 2; 3; 0; 5; 6; 7] = true 
+let test5 = contain_zero [1; 2; 3; 4; 5; 6; 7] = false 
