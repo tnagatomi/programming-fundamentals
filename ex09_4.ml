2a38cc69c1fa9bcb32462476645196ffcb64c6be
tommy <tommyt6073@gmail.com>
2018-06-23 10:20:16 +0900

    add: ex09_4 length のテスト

--- ex09_4.ml
+++ ex09_4.ml
@@ -7,3 +7,9 @@
 (* 目的：受け取ったリスト lst の長さを返す *)
 (* length : int lst -> int *)
 let length lst = 0
+
+(* テスト *)
+let test1 = length [] = 0
+let test2 = length [2] = 1
+let test3 = length [1; 3] = 2
+let test4 = length [1; 2; 3; 4; 5] = 5
