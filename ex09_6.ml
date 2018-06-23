b08ed4c13c5e7c02b1209ac5624c838ad866157e
tommy <tommyt6073@gmail.com>
2018-06-23 10:56:45 +0900

    add: ex09_6 concat のテスト

--- ex09_6.ml
+++ ex09_6.ml
@@ -1,3 +1,9 @@
 (* 目的：要素を前から順にくっつけた文字列を返す *)
 (* concat : string list -> string *)
-let concat lst = "";
+let concat lst = ""
+
+(* テスト *)
+let test1 = concat [] = ""
+let test2 = concat ["a"] = "a"
+let test3 = concat ["a"; "b"] = "ab"
+let test4 = concat ["abc"; "fg"; "xyz"] = "abcfgxyz"
