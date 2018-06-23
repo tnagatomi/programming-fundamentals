1a0aaedf838d681739f5f7abf628be68b7a77eca
tommy <tommyt6073@gmail.com>
2018-06-23 11:38:27 +0900

    add: ex09_7 count_ketsueki_A のデータの例

--- ex09_7.ml
+++ ex09_7.ml
@@ -1,5 +1,12 @@
 #use "ex08_3.ml"
 
+(* person_t list 型のデータの例 *)
+let lst1 = []
+let lst2 = [person1]
+let lst3 = [person2]
+let lst4 = [person1; person2; person3]
+let lst5 = [person2; person1; person1]
+
 (* 目的：血液型がA型の人の数を返す *)
 (* count_ketsueki_A : person_t list -> int *)
 let count_ketsueki_A lst = 0
