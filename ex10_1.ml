b8dcd9a3f7c131b8ccbb78126b9febef8acf16fa
tommy <tommyt6073@gmail.com>
2018-06-23 17:25:43 +0900

    add: ex10_1 insert のテスト

--- ex10_1.ml
+++ ex10_1.ml
@@ -1,3 +1,8 @@
 (* 目的：整数を、昇順となる位置に挿入したリストを返す *)
 (* insert: int list -> int -> int list *)
 let insert lst n = []
+
+(* テスト *)
+let test1 = insert 1 [] = [1]
+let test2 = insert 4 [1] = [1; 4]
+let test3 = insert 4 [1; 2; 5] = [1; 2; 4; 5]
