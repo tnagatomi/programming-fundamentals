c8c7eb222b215cd70c2638397ab19d2092d5221f
tommy <tommyt6073@gmail.com>
2018-06-24 13:56:10 +0900

    add: ex10_11

---
+++ ex10_11.ml
@@ -0,0 +1,15 @@
+#use "ex09_10.ml" (* ekikan_t, global_ekikan_list の定義 *) 
+
+(* 目的：2駅間の距離を返す *)
+(* get_ekikan_kyori: string -> string -> ekikan_t list -> float *)
+let rec ekikan_kyori eki1 eki2 ekikan_list = match ekikan_list with
+    [] -> infinity
+  | {kiten=kiten; shuten=shuten; keiyu=k; kyori=kyori; jikan=j} :: rest ->
+      if kiten = eki1 && shuten = eki2 then kyori
+      else if kiten = eki2 && shuten = eki1 then kyori
+      else ekikan_kyori eki1 eki2 rest
+
+(* テスト *)
+let test = ekikan_kyori "茗荷谷" "新大塚" global_ekikan_list = 1.2
+let test = ekikan_kyori "新大塚" "茗荷谷" global_ekikan_list = 1.2
+let test = ekikan_kyori "池袋" "平和台" global_ekikan_list = infinity
